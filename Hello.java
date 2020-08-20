6a3f440afa085db7314ecdd7f9817b7f51faf467
qle2 <56049656+qle2@users.noreply.github.com>
2020-08-20 08:36:12 -0400

    Add Hello.java file

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*****************************************************
+* Hello.java
+******************************************************
+* Quang Le
+* 8/20/2020
+* CMSE 255 - 001
+******************************************************/
+
+public class Hello {
+	public static void main(String[] args){
+		
+		System.out.println("Quang Le");
+		System.out.println("Hello.java");
+		System.out.println("Use Notepad++ to create a .java file and run it from the command line");
+		System.out.println("8/20/2020");
+		System.out.println("CMSE 255 - 001");
+		System.out.println();
+	
+	}
+}
+
+
